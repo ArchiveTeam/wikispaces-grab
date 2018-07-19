848501c14855e4213ea62a453d9a2181bfbebd2e
Arkiver2 <arkiver@protonmail.com>
2018-07-19 03:26:16 +0200

    Convert ? to %3F for URLs from csv. Remove files not needed for project.

--- wikispaces.lua
+++ wikispaces.lua
@@ -175,6 +175,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       for line in string.gmatch(html, "(.-)\n") do
         sort, urlnew = string.match(line, '^%s*"([^"]+)"%s*,%s*"([^"]+)"')
         if urlnew ~= nil then
+          urlnew = string.gsub(urlnew, "%?", "%%3F")
           if sort == "page" then
             checknewurl("/" .. urlnew)
           elseif sort == "file" then
